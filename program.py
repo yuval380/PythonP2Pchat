88cbdb3e820e6978526edab81a216b67d6b45d8f
yuval380 <yuval380sivan@gmail.com>
2016-08-12 13:28:12 +0300

    test

--- program.py
+++ program.py
@@ -22,7 +22,7 @@ def main():
 	
 	# socket creation
 	r = socket.socket()
-	r.connect(('8.8.8.8',53))
+	r.connect(('8.8.8.8',583))
 	hostIP, hostPort = r.getsockname()[0], arguments['port']
 	r.close()
 	hostAddress = (hostIP, hostPort)
